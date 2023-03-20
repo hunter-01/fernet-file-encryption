361b42006086826ec330eac8781a970d2b133a80
Hunter <61070982+hunter-01@users.noreply.github.com>
2023-03-20 10:48:54 -0300

    Update decryptFile.py

--- decryptFile.py
+++ decryptFile.py
@@ -13,28 +13,28 @@ if sys.argv[2] == None:
     print("No Key")
     exit()
 
-# Definindo arquivo original
+# Definindo arquivo original | Defining original file
 arqOriginal = sys.argv[1] #'testeZip.zip'
 
-# Pegando a chave
+# Pegando a chave | Taking the key
 key = sys.argv[2]#''
 
 #with open('key.txt', 'rb') as file:
 #    key = file.read()
 
-# Criando objeto Fernet com a chave
+# Criando objeto Fernet com a chave | Creating Fernet object with key
 fernet = Fernet(key)
 
-# Pegando conteúdo do arquivo original
+# Pegando conteúdo do arquivo original | Getting content from the original file
 cOriginal = ''
 
 with open(arqOriginal, 'rb') as file:
     cOriginal = file.read()
 
-# Descriptografando conteúdo
+# Descriptografando conteúdo | Decrypting content
 decrypted = fernet.decrypt(cOriginal)
 
-# Substituindo conteúdo do arquivo original
+# Substituindo conteúdo do arquivo original | Replacing original file content
 with open(arqOriginal, 'wb') as file:
     file.write(decrypted)
 
