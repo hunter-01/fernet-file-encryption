1a9c74ed2259b3adf8737e5c78732846c56087d4
Hunter <61070982+hunter-01@users.noreply.github.com>
2023-03-20 10:41:51 -0300

    Update createKey.py

--- createKey.py
+++ createKey.py
@@ -1,11 +1,11 @@
 from cryptography.fernet import Fernet
 
-# Gerar chave
+# Gerar chave | Generate key
 key = Fernet.generate_key()
 
-# Armazenar chave em arquivo
+# Armazenar chave em arquivo | Store key in file
 with open('log-keys.txt', 'ab') as file:
     file.write(key+bytes("\n","utf8"))
 
-# Mostrar key
+# Mostrar key | Show key
 print("New Key:"+str(key)+"\n Stored in log-keys.txt")
